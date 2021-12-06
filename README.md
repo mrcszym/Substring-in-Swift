d9c0e9d0045856976ea306a57cd53ec9cbabf589
Szymon <marciniecszymon00@gmail.com>
2021-12-06 11:00:38 +0100

    all in english

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Substring in Swift
  
+Program that will load two sequences of integers (first has to be longer than second) and check if the shorter one is a substring of the longer one.  If so, it will print number of appearances and indexes with they starts.
--- substring.swift
+++ substring.swift
@@ -1,7 +1,3 @@
-// Program that will load two sequences of integers (first has to be longer than second) and check
-// if the shorter one is a substring of the longer one. 
-// If so, it will print number of appearances and indexes with they starts.
-
 import Foundation
  
 print("First numerical sequence:")
